267c491b5649b30ac4bc418e395ce03edd7b8bef
MoonXCode <89719048+MoonXCode@users.noreply.github.com>
2022-02-26 16:48:17 +0300

    added string comparison function

--- scr/strlib.asm
+++ scr/strlib.asm
@@ -35,13 +35,15 @@
 start:; callable functions are used with < _ > prefix
 	MOV   DI,offset str3
 	MOV   AL,'e'
-	CALL  strchr_short
+	CALL  strchr
 	CALL  exit_program
 
 	;#.The main functions of the library (7 functions)
+; Incoming: DI, AL
+; Ret: AX-cmp result        |! al - search-symbol, bx, cx-strlen
 strcmp proc                          ; [unsafe]-Put symbols on the screen
 	CLD                          ; Set direction of increment
-	PUSH  DI
+	PUSH  DI                     ; To save DI string pointer
 	CALL  strlen
 	INC   CX
 	POP   DI
@@ -66,18 +68,20 @@ _strcmp proc                         ;
 	RET
 	ENDP
 
+; Incoming: BX, AL, CX
+; Ret: BX-symbol index      |! al - search-symbol, bx, cx-strlen 
 strchr proc                          ; [unsafe]-Search symbol
 	CLD                          ; Set direction of increment
 	XOR   BX,BX                  ; Will be the index of the found character
 	CALL  strlen                 ; In cx we write the length of the string
 	PUSH  AX
-	CALL  _strchr
+	CALL  _strchr                ; Call executor
 	POP   AX
 	RET
 	ENDP
-_strchr proc
-	MOV   bp,sp
-	MOV   AX,[bp + 2D]
+_strchr proc                         ; [cdecl] doesn-t clear anything
+	MOV   bp,sp                  ; Set stack save-pointer
+	MOV   AX,[bp + 2D]           ; Search-symbol
 @@continueSearch:
 	scasb
 	JE    @@end
@@ -89,21 +93,21 @@ _strchr proc
 	ENDP
 
 ; Incoming: SI, AL
-; Ret: Nothing
+; Ret: Nothing              |! al - end-of-line char, {bx} - string ptr, si - counter di - pos on screen
 print proc                          ; [unsafe]-Put symbols on the screen
-	XOR   AL,AL                 ; #cdecl wrapper
-	XOR   SI,SI
-	PUSH  DI
+	XOR   AL,AL                 ; #pascal wrapper
+	XOR   SI,SI                 ; Set begin of the string
+	PUSH  DI                    ; Put position on screen
 	CALL  _print
 	RET
 	ENDP
-_print proc
+_print proc                         ; [pascal]  clear di position
 	MOV   bp,sp
 	MOV   di,[bp + 2d]          ; Use di-index
 @@repeat:
-	scasb
+	scasb                       ; Cmp with end of line char
 	JE    @@end
-	INC   DI
+	INC   DI                    ; Next pos on screen
 	MOV   AL,[bx + SI]          ; Copy si-symbol to AL
 	MOV   es:[di],AL            ; Puts it on the screen
 	XOR   AL, AL
@@ -114,41 +118,40 @@ _print proc
 	RET
 	ENDP
 
-; Incoming: SI, DI, AL
-; Ret: new-string
+; Ret: new-string {di pointer}
+; Incoming: SI, DI, AL      |! al - end-of-line character, {si, di} - ptr of strings
 strncpy proc                        ; [safe]-copy a string to another
 	CLD                         ; Set direction of increment
-	XOR   AL,AL
-	CALL  _strncpy
+	CALL  _strncpy              ; Call executor
 	RET
 	ENDP
 _strncpy proc
 @@repeat:
 	movsb                       ; Copies a byte from DS:[SI] to ES:[DI]
 	CMP   es:[DI],AL
-	JE    @@end                 ; Check strings endings 
-	CMP   ds:[SI],AL            ;
-	JE    @@end                 ;
+	JE    @@end                 ; Check string №1 ending
+	CMP   ds:[SI],AL            ; Check string №2 ending
+	JE    @@end                 ; Stop copy
 	JMP   @@repeat              ; Repeat until one of the lines ends
 @@end:
 	RET
 	ENDP
 
 ; Incoming: AL,CX   
-; Ret: CX -sym.counter
+; Ret: CX -sym.counter      |! al - end-of-line character, {di} - ptr to string
 strlen_short proc                   ; [unsave]-string length
 	PUSH  AX                    ; #cdecl wrapper
 	CLD                         ; Set direction of increment
-	MOV   CX,0FFFFH             ; [!] Subtract the length of the string from the maximum
-	XOR   AL,AL                 ;     index and take the complement of two
-	CALL   _strlen_short
+	MOV   CX,0FFFFH             ; [!] | The function subtracts one from the maximum value of the
+	XOR   AL,AL                 ;     | word until it finds the desired character, and then subtracts 
+	CALL   _strlen_short        ;     | the resulting value from the maximum value of the word
 	SUB   CX,1D                 ; if you do not subtract one, then there will be a
 	POP   AX                    ; length of the string with zero
 	RET
 	ENDP
 _strlen_short proc                  ; Execute
 	repne scasb                 ; cmp al, es:[di] ~ word-ptr
-	NOT   CX                    ; [!] take the complement of two
+	NOT   CX                    ; [!] | take the complement of two
 	RET
 	ENDP
 
@@ -158,37 +161,36 @@ strlen proc                         ; [unsave]-string length
 	PUSH  AX                    ; #cdecl wrapper
 	CLD                         ; Set direction of increment
 	XOR   CX,CX                 ; Prepare parameters
-	XOR   AL,AL                 ;
-	CALL  _strlen
+	CALL  _strlen               ; Call executor
 	POP   AX
 	RET
 	ENDP
 _strlen proc                        ; Execute
-@@find_nul:
+@@find_nul:                         ; Looking for a symbol until we find
 	scasb                       ; cmp al, es:[di] ~ word-ptr
 	JE    @@end                 ; End if we find symbol
-	INC   CX
+	INC   CX                    ; Continue search
 	JMP   @@find_nul
-@@end:
+@@end:                              ; End searcing
 	RET
 	ENDP
 
 	;#.utilities functions for library (with wrappers for some)
 prog_delay proc                     ; [safe]-program delay until the key is pressed
 	PUSH  AX                    ; #cdecl wrapper 
-	JMP   _pause
+	JMP   _pause                ; Call executor
 	POP   AX
 	RET
 	ENDP
 _pause proc                         ; Simple pause with cathcing symbol
-	MOV   AX,PAUSE_VAL          
+	MOV   AX,PAUSE_VAL          ; = 0D
 	INT   16H                   ; Using the 16th interrupt to read a character
 	RET
 	ENDP
 
 set_video proc                      ; [safe]-video memory pointer.
 	PUSH  AX                    ; #cdecl wrapper 
-	CALL  _video_ptr
+	CALL  _video_ptr            ; Call executor
 	POP   AX
 	RET
 	ENDP
@@ -200,11 +202,10 @@ _video_ptr proc
 	ENDP
 
 exit_program proc                   ; #programm finalist.
-	MOV   AX,EXIT_CODE          ; (uses interrupt 21)
-	INT   21H
+	MOV   AX,EXIT_CODE          ; (uses interrupt 21) = 04C00H
+	INT   21H                   ; interrupt
 	RET
 	ENDP
-
 
 str1 db 'meow meow meow purr!', 0
 str2 db 'abobus.86-64', 0
